28a10db78963d9c6fd900e9b9acf08610654873b
MoFares7 <faresdabbas1@gmail.com>
2022-09-26 18:57:58 +0300

    initial question card

--- lib/views/screens/quiz/components/main_body_questions.dart
+++ lib/views/screens/quiz/components/main_body_questions.dart
@@ -1,4 +1,5 @@
 import 'package:quiz_app/config/constants.dart';
+import 'package:quiz_app/models/questions.dart';
 import 'package:quiz_app/views/screens/quiz/components/progress_bar.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 import 'package:flutter/material.dart';
@@ -38,11 +39,88 @@ class MainBody extends StatelessWidget {
                     ],
                   ),
                 ),
+                const Divider(
+                  thickness: 1.5,
+                ),
+                const SizedBox(
+                  height: kDefaultPadding,
+                ),
+                QuestionCard(),
               ],
             ),
           ),
         )
       ],
+    );
+  }
+}
+
+class QuestionCard extends StatelessWidget {
+  const QuestionCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25), color: Colors.white),
+      child: Column(
+        children: [
+          Text(
+            sample_data[0]['question'],
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                ?.copyWith(color: kBlackColor),
+          ),
+          SizedBox(
+            height: kDefaultPadding / 2,
+          ),
+          Options(),
+          Options(),
+          Options(),
+          Options(),
+        ],
+      ),
+    );
+  }
+}
+
+class Options extends StatelessWidget {
+  const Options({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: kDefaultPadding),
+      padding: const EdgeInsets.all(kDefaultPadding),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: kGrayColor),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            '1. Test',
+            style: TextStyle(color: kGrayColor, fontSize: 16),
+          ),
+          Container(
+            height: 26,
+            width: 26,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(
+                color: kGrayColor,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
