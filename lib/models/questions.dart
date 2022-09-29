5126c19878b4178ced4c4e1d5060e35364cf0c58
MoFares7 <faresdabbas1@gmail.com>
2022-09-29 19:52:45 +0300

    add dataset questions

--- lib/models/questions.dart
+++ lib/models/questions.dart
@@ -8,8 +8,8 @@ class Question {
   Question(
       {required this.id,
       required this.question,
-      required this.answer,
-      required this.options});
+      required this.options,
+      required this.answer});
 }
 
 const List sample_data = [
@@ -17,7 +17,7 @@ const List sample_data = [
     "id": 1,
     "question": "What is the capital of Germany ______",
     "options": ['Berlin', 'Paris', 'Liverpool', 'Cairo'],
-    "answer_index": 1,
+    "answer_index": 0,
   },
   {
     "id": 2,
@@ -28,13 +28,13 @@ const List sample_data = [
       'Mohamad bn Salman',
       'Fahd bn Salman'
     ],
-    "answer_index": 3,
+    "answer_index": 2,
   },
   {
     "id": 3,
     "question": "Where is New Delhi located _____",
     "options": ['China', 'India', 'Jaban', 'Bakstan'],
-    "answer_index": 2,
+    "answer_index": 1,
   },
   {
     "id": 4,
@@ -46,16 +46,22 @@ const List sample_data = [
       'Maechile jackson',
       'Ahmad Ali'
     ],
-    "answer_index": 1,
+    "answer_index": 0,
   },
   {
     "id": 5,
     "question": "In what year was the Suez Canal founded _____",
     "options": ['1953', '1954', '1955', '1956'],
-    "answer_index": 4,
+    "answer_index": 3,
   },
   {
     "id": 6,
+    "question": "Who won the World Cup in 1998 _____",
+    "options": ['Faranc', 'Germany', 'Brazel', 'Italy'],
+    "answer_index": 0,
+  },
+  {
+    "id": 7,
     "question": "Who is the last of the Othoman sultans _____",
     "options": [
       'Suliman Kanone',
@@ -63,12 +69,24 @@ const List sample_data = [
       'Mehmed Vahideddin',
       'Abd Alhamed'
     ],
-    "answer_index": 3,
+    "answer_index": 2,
   },
   {
-    "id": 7,
-    "question": "In what year was the internet invented? _____",
+    "id": 8,
+    "question": "In what year was the internet invented _____",
     "options": ['1982', '1983', '1984', '1985'],
+    "answer_index": 1,
+  },
+  {
+    "id": 9,
+    "question": "What is the name of the current US president _____",
+    "options": ['Obama', 'Trmp', 'Baiedn', 'Bosh'],
     "answer_index": 2,
+  },
+  {
+    "id": 10,
+    "question": "Where was the 2002 World Cup held _____",
+    "options": ['Russia', 'Jabance', 'Us', 'Germany'],
+    "answer_index": 3,
   },
 ];
